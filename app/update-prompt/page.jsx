ce58cbe5dfc81eb891e8d76a33aed039b627f512
omkarkhot0500 <omkarkhot0500@gmail.com>
2024-10-19 19:00:13 +0530

    commit

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,7 +1,6 @@
 "use client";
-
 import dynamic from "next/dynamic";
-import { useEffect, useState } from "react";
+import { useEffect, useState, Suspense } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import Form from "@components/Form";
 
@@ -12,7 +11,6 @@ const UpdatePrompt = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
   const promptId = searchParams.get("id");
-
   const [post, setPost] = useState({ prompt: "", tag: "" });
   const [submitting, setIsSubmitting] = useState(false);
 
@@ -20,22 +18,18 @@ const UpdatePrompt = () => {
     const getPromptDetails = async () => {
       const response = await fetch(`/api/prompt/${promptId}`);
       const data = await response.json();
-
       setPost({
         prompt: data.prompt,
         tag: data.tag,
       });
     };
-
     if (promptId) getPromptDetails();
   }, [promptId]);
 
   const updatePrompt = async (e) => {
     e.preventDefault();
     setIsSubmitting(true);
-
     if (!promptId) return alert("Missing PromptId!");
-
     try {
       const response = await fetch(`/api/prompt/${promptId}`, {
         method: "PATCH",
@@ -44,7 +38,6 @@ const UpdatePrompt = () => {
           tag: post.tag,
         }),
       });
-
       if (response.ok) {
         router.push("/");
       }
@@ -56,13 +49,15 @@ const UpdatePrompt = () => {
   };
 
   return (
-    <Form
-      type="Edit"
-      post={post}
-      setPost={setPost}
-      submitting={submitting}
-      handleSubmit={updatePrompt}
-    />
+    <Suspense fallback={<div>Loading...</div>}>
+      <Form
+        type="Edit"
+        post={post}
+        setPost={setPost}
+        submitting={submitting}
+        handleSubmit={updatePrompt}
+      />
+    </Suspense>
   );
 };
 
